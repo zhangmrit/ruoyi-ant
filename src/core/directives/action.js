93eebbed9cab28170ffc925eedf6bdfe30fc16da
zhangmrit <zhangmrit@126.com>
2019-06-11 10:51:21 +0800

    调整权限指令

--- src/core/directives/action.js
+++ src/core/directives/action.js
@@ -2,33 +2,34 @@ import Vue from 'vue'
 import store from '@/store'
 
 /**
- * Action 权限指令
- * 指令用法：
- *  - 在需要控制 action 级别权限的组件上使用 v-action:[method] , 如下：
- *    <i-button v-action:add >添加用户</a-button>
- *    <a-button v-action:delete>删除用户</a-button>
- *    <a v-action:edit @click="edit(record)">修改</a>
- *
- *  - 当前用户没有权限时，组件上使用了该指令则会被隐藏
- *  - 当后台权限跟 pro 提供的模式不同时，只需要针对这里的权限过滤进行修改即可
- *
- *  @see https://github.com/sendya/ant-design-pro-vue/pull/53
+ * has 权限指令
+ * 指令用法：参看shiro
+ *    <a-button v-has="'user:add'" >添加用户</a-button>
+ *    <a-button v-has="'user:edit'">删除用户</a-button>
+ *    <a v-has="'user:del'" @click="edit(record)">修改</a>
  */
-const action = Vue.directive('action', {
-  inserted: function (el, binding, vnode) {
-    const actionName = binding.arg
-    const roles = store.getters.roles
-    const elVal = vnode.context.$route.meta.permission
-    const permissionId = elVal instanceof String && [elVal] || elVal
-    roles.permissions.forEach(p => {
-      if (!permissionId.includes(p.permissionId)) {
-        return
-      }
-      if (p.actionList && !p.actionList.includes(actionName)) {
-        el.parentNode && el.parentNode.removeChild(el) || (el.style.display = 'none')
-      }
-    })
+const has = Vue.directive('has', {
+  bind: function (el, binding, vnode) {
+    if (!Vue.prototype.$_has(binding.value)) {
+      el.parentNode && el.parentNode.removeChild(el) || (el.style.display = 'none')
+    }
   }
 })
-
-export default action
+// 权限检查方法
+Vue.prototype.$_has = function (value) {
+  // 获取用户按钮权限
+  let isExist = false
+  const dynamicButtons = store.getters.buttons
+  if (dynamicButtons === undefined || dynamicButtons === null || dynamicButtons.length < 1) {
+    return isExist
+  }
+  dynamicButtons.forEach(button => {
+    if (button === value) {
+      isExist = true
+      return isExist
+    }
+  })
+  // return isExist
+  return true
+}
+export default has
--- src/permission.js
+++ src/permission.js
@@ -65,38 +65,3 @@ router.beforeEach((to, from, next) => {
 router.afterEach(() => {
   NProgress.done() // finish progress bar
 })
-
-/**
- * has 权限指令
- * 指令用法：参看shiro
- *    <a-button v-has="'user:add'" >添加用户</a-button>
- *    <a-button v-has="'user:edit'">删除用户</a-button>
- *    <a v-has="'user:del'" @click="edit(record)">修改</a>
- */
-const has = Vue.directive('has', {
-  bind: function (el, binding, vnode) {
-    if (!Vue.prototype.$_has(binding.value)) {
-      el.parentNode && el.parentNode.removeChild(el) || (el.style.display = 'none')
-    }
-  }
-})
-// 权限检查方法
-Vue.prototype.$_has = function (value) {
-  // 获取用户按钮权限
-  let isExist = false
-  const dynamicButtons = store.getters.buttons
-  if (dynamicButtons === undefined || dynamicButtons === null || dynamicButtons.length < 1) {
-    return isExist
-  }
-  dynamicButtons.forEach(button => {
-    if (button === value) {
-      isExist = true
-      return isExist
-    }
-  })
-  // return isExist
-  return true
-}
-export {
-  has
-}
