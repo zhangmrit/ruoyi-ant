be7e2c150c3331aad1b24d5d29ca447afc8dc5f0
zhangmrit <zhangmrit@126.com>
2019-08-20 11:58:49 +0800

    删除多余代码

--- src/utils/download.js
+++ src/utils/download.js
@@ -14,29 +14,9 @@ export function exportPost (url, params) {
       download(res.msg)
     }
   })
-
-  // if (res.code === 0) {
-  // const aLink = document.createElement('a')
-  // const blob = new Blob([res.data], { type: 'application/vnd.ms-excel' })
-  // aLink.href = URL.createObjectURL(blob)
-  // aLink.setAttribute('download', res.msg) // 设置下载文件名称
-  // aLink.click()
-  // document.body.appendChild(aLink)
-  // this.$refs.loadElement.appendChild(aLink)
-  // window.open(baseURL + downloadUrl + '?fileName=' + res.msg)
-  // return axios({
-  //   url: downloadUrl, // 接口名字
-  //   method: 'get',
-  //   params: { fileName: res.msg }
-  //   // responseType: 'blob'// 通过api应该知道这是什么吧
-  // })
-  // }
-  // })
 }
 
 export function download (filename) {
-  // eslint-disable-next-line no-debugger
-  // debugger
   const token = Vue.ls.get(ACCESS_TOKEN)
   pureAxios({
     url: downloadUrl,
@@ -48,8 +28,7 @@ export function download (filename) {
     }
   }).then(res => {
     const aLink = document.createElement('a')
-    // const { fileName } = res.headers
-    var blob = new Blob([res.data], { type: 'application/octet-binary' })
+    var blob = new Blob([res.data], { type: 'application/octet-stream' })
     // //从response的headers中获取filename, 后端response.setHeader("Content-disposition", "attachment; fileName=xxxx.docx") 设置的文件名;
     var patt = new RegExp('fileName=([^;]+\\.[^\\.;]+);*')
     var contentDisposition = decodeURI(res.headers['content-disposition'])
