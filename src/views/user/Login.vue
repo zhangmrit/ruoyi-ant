cf583760359252cb9bf0499917c8f5c2de6946d1
wind <zhangmrit@126.com>
2019-09-05 15:09:40 +0800

    解决vue router 报： Uncaught (in promise)

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -277,15 +277,13 @@ export default {
     },
     loginSuccess (res) {
       if (res.code === 0) {
-        this.$router.push({ name: 'dashboard' })
-        this.isLoginError = false
-        // 延迟 1 秒显示欢迎信息
-        setTimeout(() => {
+        this.$router.push({ name: 'dashboard' }, () => {
           this.$notification.success({
             message: '欢迎',
             description: `${timeFix()}，欢迎回来`
           })
-        }, 1000)
+        })
+        this.isLoginError = false
       } else {
         this.requestFailed(res)
       }
