2f625ababf2ff1ed42ce27152d42ebe25f27a8b5
wind <zhangmrit@126.com>
2020-03-09 14:11:57 +0800

    feat: add pageView actions callback

--- src/views/list/CardList.vue
+++ src/views/list/CardList.vue
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+
 const dataSource = []
 dataSource.push({})
 for (let i = 0; i < 11; i++) {
@@ -41,18 +42,34 @@ for (let i = 0; i < 11; i++) {
     content: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
   })
 }
+
 export default {
   name: 'CardList',
   data () {
     return {
       description: '段落示意：蚂蚁金服务设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态， 提供跨越设计与开发的体验解决方案。',
       linkList: [
-        { icon: 'rocket', href: '#', title: '快速开始' },
+        {
+          icon: 'rocket',
+          href: '#',
+          title: '快速开始',
+          // 回调，可不写
+          callback: () => {
+            // this.$message.info('快速开始被单击')
+            this.testFun()
+            console.log('call[\'快速开始\'] action')
+          }
+        },
         { icon: 'info-circle-o', href: '#', title: '产品简介' },
         { icon: 'file-text', href: '#', title: '产品文档' }
       ],
       extraImage: 'https://gw.alipayobjects.com/zos/rmsportal/RzwpdLnhmvDJToTdfDPe.png',
       dataSource
+    }
+  },
+  methods: {
+    testFun () {
+      this.$message.info('快速开始被点击！')
     }
   }
 }
@@ -60,20 +77,24 @@ export default {
 
 <style lang="less" scoped>
   @import "~@/components/index.less";
+
   .card-list {
     /deep/ .ant-card-body:hover {
       .ant-card-meta-title>a {
         color: @primary-color;
       }
     }
+
     /deep/ .ant-card-meta-title {
       margin-bottom: 12px;
+
       &>a {
         display: inline-block;
         max-width: 100%;
         color: rgba(0,0,0,.85);
       }
     }
+
     /deep/ .meta-content {
       position: relative;
       overflow: hidden;
@@ -82,25 +103,31 @@ export default {
       height: 64px;
       -webkit-line-clamp: 3;
       -webkit-box-orient: vertical;
+
       margin-bottom: 1em;
     }
   }
+
   .card-avatar {
     width: 48px;
     height: 48px;
     border-radius: 48px;
   }
+
   .ant-card-actions {
     background: #f7f9fa;
+
     li {
       float: left;
       text-align: center;
       margin: 12px 0;
       color: rgba(0, 0, 0, 0.45);
       width: 50%;
+
       &:not(:last-child) {
         border-right: 1px solid #e8e8e8;
       }
+
       a {
         color: rgba(0, 0, 0, .45);
         line-height: 22px;
@@ -112,10 +139,12 @@ export default {
       }
     }
   }
+
   .new-btn {
     background-color: #fff;
     border-radius: 2px;
     width: 100%;
     height: 188px;
   }
+
 </style>
