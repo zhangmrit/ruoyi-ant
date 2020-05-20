7258046c18b2e1ed75dc0f4bbb78b447c9d9a16d
wind <zhangmrit@126.com>
2020-05-20 15:26:56 +0800

    fix: rightContent render

--- src/components/GlobalHeader/RightContent.vue
+++ src/components/GlobalHeader/RightContent.vue
@@ -1,10 +1,10 @@
 <template>
   <div :class="wrpCls">
-    <a href="http://doc.rycloud.zmrit.com/#/quick-start" target="_blank" :class="actionClassName">
+    <a href="http://doc.rycloud.zmrit.com/#/quick-start" target="_blank" :class="prefixCls">
       <a-icon type="question-circle-o"></a-icon>
     </a>
-    <select-lang :class="actionClassName" />
-    <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="actionClassName" />
+    <select-lang :class="prefixCls" />
+    <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="prefixCls" />
   </div>
 </template>
 
@@ -19,6 +19,14 @@ export default {
     SelectLang
   },
   props: {
+    prefixCls: {
+      type: String,
+      default: 'ant-pro-global-header-index-action'
+    },
+    isMobile: {
+      type: Boolean,
+      default: () => false
+    },
     topMenu: {
       type: Boolean,
       required: true
@@ -29,7 +37,6 @@ export default {
     }
   },
   data () {
-    this.actionClassName = 'ant-pro-global-header-index-action'
     return {
       showMenu: true,
       currentUser: {}
@@ -39,7 +46,7 @@ export default {
     wrpCls () {
       return {
         'ant-pro-global-header-index-right': true,
-        [`ant-pro-global-header-index-${this.topMenu && this.theme || 'light'}`]: true
+        [`ant-pro-global-header-index-${(this.topMenu && this.isMobile) && this.theme || 'light'}`]: true
       }
     }
   },
