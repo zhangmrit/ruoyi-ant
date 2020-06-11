a1a6182efd27b76a222002c3036e564b152929bd
zhangmrit <zhangmrit@126.com>
2020-06-11 23:47:42 +0800

    密码框组件

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -35,10 +35,8 @@
           </a-form-item>
 
           <a-form-item>
-            <a-input
+            <a-input-password
               size="large"
-              type="password"
-              autocomplete="false"
               placeholder="密码: admin123"
               v-decorator="[
                 'password',
@@ -46,7 +44,7 @@
               ]"
             >
               <a-icon slot="prefix" type="lock" :style="{ color: 'rgba(0,0,0,.25)' }" />
-            </a-input>
+            </a-input-password>
           </a-form-item>
         </a-tab-pane>
         <a-tab-pane key="tab2" tab="手机号登录">
