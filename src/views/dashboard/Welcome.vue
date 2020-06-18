87ddd04dc75f47e69387cd52c03eddd4adfc6c1d
zhangmrit <zhangmrit@126.com>
2020-06-18 23:12:17 +0800

    阿里云618活动

--- src/views/dashboard/Welcome.vue
+++ src/views/dashboard/Welcome.vue
@@ -3,11 +3,12 @@
     <template v-slot:content>
       <div class="page-header-content">
         <div class="avatar">
-          <a-avatar size="large" :src="currentUser.avatar"/>
+          <a-avatar size="large" :src="currentUser.avatar" />
         </div>
         <div class="content">
           <div class="content-title">
-            {{ timeFix }}，{{ user.name }}<span class="welcome-text">，{{ welcome }}</span>
+            {{ timeFix }}，{{ user.name }}
+            <span class="welcome-text">，{{ welcome }}</span>
           </div>
           <div>前端工程师 | 蚂蚁金服 - 某某某事业群 - VUE平台</div>
         </div>
@@ -31,23 +32,49 @@
       <a-row :gutter="24">
         <a-col :xl="16" :lg="24" :md="24" :sm="24" :xs="24">
           <a-card :bordered="false" title="RuoYi Cloud">
-            <p><strong>Spring Cloud</strong> 版本的若依,集成 <strong>Spring Cloud</strong> 的几大神兽: <strong>eureka,gateway,hystrix,feign,config... </strong>自定义token实现授权，直接存到redis,集成工作activiti,另有nacos版本</p>
-            <p><strong>文档地址 </strong><a href="http://doc.rycloud.zmrit.com" target="_blank">http://doc.rycloud.zmrit.com</a></p>
-            <p><strong>RuoYi Cloud</strong> 承诺永久开源，不会出商业版之类的东西</p>
-            <p><strong>RuoYi Cloud</strong> 是采用 MIT 许可的开源项目，使用完全免费。 但为了项目能够健康持续的发展下去，我们期望获得相应的资金支持。 你可以通过下列的方法来赞助我们的开发。</p>
+            <p>
+              <strong>Spring Cloud</strong> 版本的若依,集成
+              <strong>Spring Cloud</strong> 的几大神兽:
+              <strong>eureka,gateway,hystrix,feign,config...</strong>自定义token实现授权，直接存到redis,集成工作activiti,另有nacos版本
+            </p>
+            <p>
+              <strong>文档地址</strong>
+              <a href="http://doc.rycloud.zmrit.com" target="_blank">http://doc.rycloud.zmrit.com</a>
+            </p>
+            <p>
+              <strong>RuoYi Cloud</strong> 承诺永久开源，不会出商业版之类的东西
+            </p>
+            <p>
+              <strong>RuoYi Cloud</strong> 是采用 MIT 许可的开源项目，使用完全免费。 但为了项目能够健康持续的发展下去，我们期望获得相应的资金支持。 你可以通过下列的方法来赞助我们的开发。
+            </p>
             <ul>
-              <li><h3>一次性赞助</h3></li>
-              <p>捐赠者信息将会在文档捐赠页展示 <router-link to="/others/donateList">捐赠名单</router-link></p>
-              <li><h3>周期性赞助</h3></li>
+              <li>
+                <h3>一次性赞助</h3>
+              </li>
+              <p>
+                捐赠者信息将会在文档捐赠页展示
+                <router-link to="/others/donateList">捐赠名单</router-link>
+              </p>
+              <li>
+                <h3>周期性赞助</h3>
+              </li>
               <p>周期性赞助可以获得额外的回报，比如你的名字会出现在 GitHub 仓库中，再比如你的公司 logo 会出现在我们的官网上</p>
             </ul>
-            <p><strong>Spring Cloud 从零开始免费教程 </strong><a href="http://club.zmrit.com/cloud-zero/" target="_blank">http://club.zmrit.com/cloud-zero/</a></p>
+            <p>
+              <strong>Spring Cloud 从零开始免费教程</strong>
+              <a
+                href="http://club.zmrit.com/cloud-zero/"
+                target="_blank"
+              >http://club.zmrit.com/cloud-zero/</a>
+            </p>
           </a-card>
 
           <a-card title="更新日志" :bordered="false">
             <a-timeline mode="left">
               <a-timeline-item :color="item.color" :key="index" v-for="(item, index) in changeList">
-                <p><strong>{{ item.title }}</strong></p>
+                <p>
+                  <strong>{{ item.title }}</strong>
+                </p>
                 <p :key="i" v-for="(c, i) in item.list" class="changList">{{ c }}</p>
               </a-timeline-item>
             </a-timeline>
@@ -60,7 +87,38 @@
           :md="24"
           :sm="24"
           :xs="24">
-          <a-card title="作者录制的启动演示视频" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
+          <a-card
+            title="阿里云活动"
+            style="margin-bottom: 24px"
+            :bordered="false"
+            :body-style="{padding: 0}"
+          >
+            <h3 style="margin: 12px 26px">【年中618，云产品0.9折起】口碑爆款，云服务器低至89.8元/年</h3>
+            <ul>
+              <li>
+                <h4>ECS突发性能型t5</h4>
+              </li>
+              <p>t5处理器：skylake，20%性能基线 1核2G 1年89 3年229</p>
+              <!-- <li>
+                <h4>ECS共享型n4-3年</h4>
+              </li>
+              <p>n4处理器：100%CPU性能 2核4G 3年799</p>
+              <li>
+                <h4>ECS突发性能t5-3年</h4>
+              </li>
+              <p>20%基准CPU计算性能，高性价比之选 2核4G 3年899</p> -->
+            </ul>
+            <h4 style="margin: 12px 26px"></h4>
+            <a href="https://www.aliyun.com/activity/618/index?userCode=p88vtdla" target="_blank">
+              <a-button type="primary" style="margin: 12px 26px">朕要瞧瞧</a-button>
+            </a>
+          </a-card>
+          <a-card
+            title="作者录制的启动演示视频"
+            style="margin-bottom: 24px"
+            :bordered="false"
+            :body-style="{padding: 0}"
+          >
             <div style="margin:1px">
               <iframe
                 src="//player.bilibili.com/player.html?aid=95044508&cid=162255217&page=1"
@@ -70,29 +128,17 @@
                 framespacing="0"
                 allowfullscreen="true"
                 style="width:100%;height:400px;"
-              >
-              </iframe>
+              ></iframe>
             </div>
-
           </a-card>
-          <a-card title="阿里云活动" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
-            <h3 style="margin: 12px 26px">【阿里云双12感恩回馈，云产品冰点价】云服务器89元/年，这里有专享低价</h3>
-            <ul>
-              <li><h4>ECS突发性能型t5</h4></li>
-              <p>t5处理器：skylake，20%性能基线 1核2G 1年89 3年229</p>
-              <li><h4>ECS共享型n4-3年</h4></li>
-              <p>n4处理器：100%CPU性能 2核4G 3年799</p>
-              <li><h4>ECS突发性能t5-3年</h4></li>
-              <p>20%基准CPU计算性能，高性价比之选 2核4G 3年899</p>
-            </ul>
-            <h4 style="margin: 12px 26px"></h4>
-            <a href="https://www.aliyun.com/minisite/goods?userCode=p88vtdla&share_source=copy_link" target="_blank">
-              <a-button type="primary" style="margin: 12px 26px">朕要瞧瞧</a-button>
-            </a>
-          </a-card>
-          <a-card title="捐赠" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
+          <a-card
+            title="捐赠"
+            style="margin-bottom: 24px"
+            :bordered="false"
+            :body-style="{padding: 0}"
+          >
             <h3 style="margin: 12px 26px">请作者喝杯咖啡或者建设演示服务器</h3>
-            <img style="width:100%" src="http://doc.rycloud.zmrit.com/images/donate.png"/>
+            <img style="width:100%" src="http://doc.rycloud.zmrit.com/images/donate.png" />
           </a-card>
           <!-- <a-card title="更新日志" style="margin-bottom: 24px" :bordered="false" :body-style="{padding: 0}">
             <a-collapse defaultActiveKey="0" accordion :bordered="false">
@@ -100,11 +146,17 @@
                 <p :key="i" v-for="(c, i) in item.list">{{ c }}</p>
               </a-collapse-panel>
             </a-collapse>
-          </a-card> -->
-          <a-card title="联系信息" :bordered="false" >
+          </a-card>-->
+          <a-card title="联系信息" :bordered="false">
             <a-row>
-              <a-col :span="4"><a-icon type="qq" />QQ群：</a-col>
-              <a-col :span="20"><a href="https://jq.qq.com/?_wv=1027&k=5JGXHPD" target="_blank"><a-button type="primary" size="small">755109875</a-button></a> 点击按钮入群</a-col>
+              <a-col :span="4">
+                <a-icon type="qq" />QQ群：
+              </a-col>
+              <a-col :span="20">
+                <a href="https://jq.qq.com/?_wv=1027&k=5JGXHPD" target="_blank">
+                  <a-button type="primary" size="small">755109875</a-button>
+                </a> 点击按钮入群
+              </a-col>
             </a-row>
           </a-card>
         </a-col>
@@ -162,11 +214,13 @@ export default {
           }
         }
       },
-      scale: [{
-        dataKey: 'score',
-        min: 0,
-        max: 80
-      }],
+      scale: [
+        {
+          dataKey: 'score',
+          min: 0,
+          max: 80
+        }
+      ],
       axisData: [
         { item: '引用', a: 70, b: 30, c: 40 },
         { item: '口碑', a: 60, b: 70, c: 40 },
@@ -176,7 +230,8 @@ export default {
         { item: '引用', a: 70, b: 50, c: 40 }
       ],
       changeList: [
-        { title: '1.1.0-SNAPSHOT 2020-03-12',
+        {
+          title: '1.1.0-SNAPSHOT 2020-03-12',
           color: 'green',
           list: [
             '根据流程文件部署流程定义',
@@ -192,7 +247,8 @@ export default {
             '动态菜单优化'
           ]
         },
-        { title: '1.0.2-SNAPSHOT 2019-09-30',
+        {
+          title: '1.0.2-SNAPSHOT 2019-09-30',
           color: 'green',
           list: [
             '优化角色授权实现联动和半选',
@@ -209,7 +265,8 @@ export default {
             'vue-cropper头像组件'
           ]
         },
-        { title: '1.0.1-SNAPSHOT 2019-08-23',
+        {
+          title: '1.0.1-SNAPSHOT 2019-08-23',
           color: 'green',
           list: [
             '新增ruoyi 4.0代码生成功能，未修改模板',
@@ -220,7 +277,8 @@ export default {
             '优化读写分离逻辑'
           ]
         },
-        { title: '1.0.0-SNAPSHOT 2019-07-25',
+        {
+          title: '1.0.0-SNAPSHOT 2019-07-25',
           color: 'green',
           list: [
             'maven坐标修改为com.ruoyi.cloud',
@@ -240,8 +298,8 @@ export default {
   },
   computed: {
     ...mapState({
-      nickname: (state) => state.user.nickname,
-      welcome: (state) => state.user.welcome
+      nickname: state => state.user.nickname,
+      welcome: state => state.user.welcome
     }),
     currentUser () {
       return {
@@ -271,59 +329,57 @@ export default {
   },
   methods: {
     getTeams () {
-      this.$http.get('/workplace/teams')
-        .then(res => {
-          this.teams = res.result
-          this.loading = false
-        })
+      this.$http.get('/workplace/teams').then(res => {
+        this.teams = res.result
+        this.loading = false
+      })
     },
     initRadar () {
       this.radarLoading = true
 
-      this.$http.get('/workplace/radar')
-        .then(res => {
-          const dv = new DataSet.View().source(res.result)
-          dv.transform({
-            type: 'fold',
-            fields: ['个人', '团队', '部门'],
-            key: 'user',
-            value: 'score'
-          })
-
-          this.radarData = dv.rows
-          this.radarLoading = false
+      this.$http.get('/workplace/radar').then(res => {
+        const dv = new DataSet.View().source(res.result)
+        dv.transform({
+          type: 'fold',
+          fields: ['个人', '团队', '部门'],
+          key: 'user',
+          value: 'score'
         })
+
+        this.radarData = dv.rows
+        this.radarLoading = false
+      })
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
- @import "./Workplace.less";
-.changList{
+@import './Workplace.less';
+.changList {
   margin-top: -14px;
 }
 .members {
-    a {
-      display: block;
-      margin: 12px 0;
+  a {
+    display: block;
+    margin: 12px 0;
+    line-height: 24px;
+    height: 24px;
+    .member {
+      font-size: 14px;
+      color: rgba(0, 0, 0, 0.65);
       line-height: 24px;
-      height: 24px;
-      .member {
-        font-size: 14px;
-        color: rgba(0, 0, 0, .65);
-        line-height: 24px;
-        max-width: 100px;
-        vertical-align: top;
-        margin-left: 12px;
-        transition: all 0.3s;
-        display: inline-block;
-      }
-      &:hover {
-        span {
-          color: #1890ff;
-        }
+      max-width: 100px;
+      vertical-align: top;
+      margin-left: 12px;
+      transition: all 0.3s;
+      display: inline-block;
+    }
+    &:hover {
+      span {
+        color: #1890ff;
       }
     }
   }
+}
 </style>
