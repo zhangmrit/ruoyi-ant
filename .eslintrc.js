356dd936c5444431ed4d2d24f01b32e91c36771c
zhangmrit <zhangmrit@126.com>
2020-07-07 16:01:18 +0800

    update des

--- .eslintrc.js
+++ .eslintrc.js
@@ -61,13 +61,15 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  overrides: [{
-    files: [
-      '**/__tests__/*.{j,t}s?(x)',
-      '**/tests/unit/**/*.spec.{j,t}s?(x)'
-    ],
-    env: {
-      jest: true
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
     }
-  }]
+  ]
 }
